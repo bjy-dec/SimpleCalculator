1e41e7a3be94dc43de0a3b190a8c8e19f6d01971
박경일 <one040422@gmail.com>
2026-03-26 15:33:04 +0900

    과제1 코딩과 테스트 완료 textbox를 두개 배치해 한개의 textbox에는 모든 입력 내용이, 나머지 textbox에는 피연산자가 하나씩만 표시되고 결과값만 표시되게 설정 button 변수이름 설정 및 더하기 연산 가

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,335 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            textBoxExpression = new TextBox();
+            textBoxResult = new TextBox();
+            label1 = new Label();
+            btnCE = new Button();
+            btnC = new Button();
+            btnDel = new Button();
+            btnDivide = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btnMultiply = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btnMinus = new Button();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btnPlus = new Button();
+            btnPM = new Button();
+            btn0 = new Button();
+            btnDot = new Button();
+            btnResult = new Button();
+            SuspendLayout();
+            // 
+            // textBoxExpression
+            // 
+            textBoxExpression.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            textBoxExpression.Location = new Point(139, 99);
+            textBoxExpression.Name = "textBoxExpression";
+            textBoxExpression.Size = new Size(391, 29);
+            textBoxExpression.TabIndex = 0;
+            textBoxExpression.TextChanged += textBoxExpression_TextChanged;
+            // 
+            // textBoxResult
+            // 
+            textBoxResult.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            textBoxResult.Location = new Point(139, 134);
+            textBoxResult.Name = "textBoxResult";
+            textBoxResult.Size = new Size(391, 29);
+            textBoxResult.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 36F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(123, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(426, 65);
+            label1.TabIndex = 2;
+            label1.Text = "Simple Calculator";
+            // 
+            // btnCE
+            // 
+            btnCE.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnCE.Location = new Point(139, 188);
+            btnCE.Name = "btnCE";
+            btnCE.Size = new Size(75, 40);
+            btnCE.TabIndex = 3;
+            btnCE.Text = "CE";
+            btnCE.UseVisualStyleBackColor = true;
+            // 
+            // btnC
+            // 
+            btnC.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnC.Location = new Point(241, 191);
+            btnC.Name = "btnC";
+            btnC.Size = new Size(75, 39);
+            btnC.TabIndex = 4;
+            btnC.Text = "C";
+            btnC.UseVisualStyleBackColor = true;
+            // 
+            // btnDel
+            // 
+            btnDel.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnDel.Location = new Point(344, 189);
+            btnDel.Name = "btnDel";
+            btnDel.Size = new Size(75, 37);
+            btnDel.TabIndex = 5;
+            btnDel.Text = "del";
+            btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            btnDivide.BackColor = SystemColors.ControlLightLight;
+            btnDivide.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnDivide.ForeColor = Color.Red;
+            btnDivide.Location = new Point(453, 189);
+            btnDivide.Name = "btnDivide";
+            btnDivide.Size = new Size(77, 37);
+            btnDivide.TabIndex = 6;
+            btnDivide.Text = "÷";
+            btnDivide.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn7.ForeColor = Color.FromArgb(0, 0, 192);
+            btn7.Location = new Point(140, 242);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(74, 36);
+            btn7.TabIndex = 7;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn8.ForeColor = Color.Blue;
+            btn8.Location = new Point(241, 242);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(75, 36);
+            btn8.TabIndex = 8;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn9.ForeColor = Color.FromArgb(0, 0, 192);
+            btn9.Location = new Point(344, 242);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(75, 36);
+            btn9.TabIndex = 9;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            btnMultiply.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnMultiply.ForeColor = Color.Red;
+            btnMultiply.Location = new Point(453, 242);
+            btnMultiply.Name = "btnMultiply";
+            btnMultiply.Size = new Size(77, 36);
+            btnMultiply.TabIndex = 10;
+            btnMultiply.Text = "X";
+            btnMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn4.ForeColor = Color.FromArgb(0, 0, 192);
+            btn4.Location = new Point(139, 292);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(75, 39);
+            btn4.TabIndex = 11;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn5.ForeColor = Color.FromArgb(0, 0, 192);
+            btn5.Location = new Point(241, 292);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(75, 39);
+            btn5.TabIndex = 12;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn6.ForeColor = Color.FromArgb(0, 0, 192);
+            btn6.Location = new Point(344, 292);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(75, 39);
+            btn6.TabIndex = 13;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            btnMinus.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnMinus.ForeColor = Color.Red;
+            btnMinus.Location = new Point(453, 292);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(77, 39);
+            btnMinus.TabIndex = 14;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn1.ForeColor = Color.FromArgb(0, 0, 192);
+            btn1.Location = new Point(140, 348);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(74, 38);
+            btn1.TabIndex = 15;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn2.ForeColor = Color.FromArgb(0, 0, 192);
+            btn2.Location = new Point(241, 348);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(75, 38);
+            btn2.TabIndex = 16;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn3.ForeColor = Color.FromArgb(0, 0, 192);
+            btn3.Location = new Point(344, 348);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(75, 38);
+            btn3.TabIndex = 17;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            // 
+            // btnPlus
+            // 
+            btnPlus.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnPlus.ForeColor = Color.Red;
+            btnPlus.Location = new Point(453, 348);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(77, 38);
+            btnPlus.TabIndex = 18;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = true;
+            // 
+            // btnPM
+            // 
+            btnPM.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnPM.ForeColor = Color.Black;
+            btnPM.Location = new Point(140, 400);
+            btnPM.Name = "btnPM";
+            btnPM.Size = new Size(74, 38);
+            btnPM.TabIndex = 19;
+            btnPM.Text = "+/-";
+            btnPM.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btn0.ForeColor = Color.FromArgb(0, 0, 192);
+            btn0.Location = new Point(241, 400);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(74, 38);
+            btn0.TabIndex = 20;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnDot
+            // 
+            btnDot.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnDot.ForeColor = Color.Black;
+            btnDot.Location = new Point(345, 400);
+            btnDot.Name = "btnDot";
+            btnDot.Size = new Size(74, 38);
+            btnDot.TabIndex = 21;
+            btnDot.Text = ".";
+            btnDot.UseVisualStyleBackColor = true;
+            // 
+            // btnResult
+            // 
+            btnResult.Font = new Font("맑은 고딕", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnResult.ForeColor = Color.Black;
+            btnResult.Location = new Point(456, 400);
+            btnResult.Name = "btnResult";
+            btnResult.Size = new Size(74, 38);
+            btnResult.TabIndex = 22;
+            btnResult.Text = "=";
+            btnResult.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnResult);
+            Controls.Add(btnDot);
+            Controls.Add(btn0);
+            Controls.Add(btnPM);
+            Controls.Add(btnPlus);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btnMinus);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btnMultiply);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btnDivide);
+            Controls.Add(btnDel);
+            Controls.Add(btnC);
+            Controls.Add(btnCE);
+            Controls.Add(label1);
+            Controls.Add(textBoxResult);
+            Controls.Add(textBoxExpression);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBoxExpression;
+        private TextBox textBoxResult;
+        private Label label1;
+        private Button btnCE;
+        private Button btnC;
+        private Button btnDel;
+        private Button btnDivide;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btnMultiply;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btnMinus;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btnPlus;
+        private Button btnPM;
+        private Button btn0;
+        private Button btnDot;
+        private Button btnResult;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,206 @@
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SimpleCalculator
 {
+
     public partial class Form1 : Form
     {
+        private string currentOperand = string.Empty;
+        private List<string> tokens = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+            AttachHandlers();
+        }
+
+        private void AttachHandlers()
+        {
+            // Digit buttons
+            btn0.Click += Digit_Click;
+            btn1.Click += Digit_Click;
+            btn2.Click += Digit_Click;
+            btn3.Click += Digit_Click;
+            btn4.Click += Digit_Click;
+            btn5.Click += Digit_Click;
+            btn6.Click += Digit_Click;
+            btn7.Click += Digit_Click;
+            btn8.Click += Digit_Click;
+            btn9.Click += Digit_Click;
+
+            // Dot
+            btnDot.Click += Dot_Click;
+
+            // Operators
+            btnPlus.Click += Operator_Click;
+            btnMinus.Click += Operator_Click;
+            btnMultiply.Click += Operator_Click;
+            btnDivide.Click += Operator_Click;
+
+            // Controls
+            btnC.Click += BtnC_Click;
+            btnCE.Click += BtnCE_Click;
+            btnDel.Click += BtnDel_Click;
+            btnPM.Click += BtnPM_Click;
+            btnResult.Click += BtnResult_Click;
+        }
+
+        private void Digit_Click(object? sender, EventArgs e)
+        {
+            if (sender is Button b)
+            {
+                // Append to expression only; textBoxExpression_TextChanged will update currentOperand and textBoxResult
+                textBoxExpression.AppendText(b.Text);
+            }
+        }
+
+        private void Dot_Click(object? sender, EventArgs e)
+        {
+            if (!currentOperand.Contains('.'))
+            {
+                // Only append to the expression; TextChanged handler will update currentOperand/result
+                if (string.IsNullOrEmpty(currentOperand))
+                {
+                    textBoxExpression.AppendText("0.");
+                }
+                else
+                {
+                    textBoxExpression.AppendText(".");
+                }
+            }
+        }
+
+        private void Operator_Click(object? sender, EventArgs e)
+        {
+            if (sender is Button b)
+            {
+                // If there is a current operand, push it first
+                if (!string.IsNullOrEmpty(currentOperand))
+                {
+                    tokens.Add(currentOperand);
+                    // show the completed operand in result (operand by operand)
+                    textBoxResult.Text = currentOperand;
+                    currentOperand = string.Empty;
+                }
+
+                // If last token is an operator, replace it
+                if (tokens.Any() && IsOperator(tokens.Last()))
+                {
+                    tokens[tokens.Count - 1] = b.Text;
+                }
+                else
+                {
+                    tokens.Add(b.Text);
+                }
+
+                textBoxExpression.AppendText(b.Text);
+            }
+        }
+
+        private void BtnC_Click(object? sender, EventArgs e)
+        {
+            // Clear all
+            currentOperand = string.Empty;
+            tokens.Clear();
+            textBoxExpression.Clear();
+            textBoxResult.Clear();
+        }
+
+        private void BtnCE_Click(object? sender, EventArgs e)
+        {
+            // Clear current entry
+            currentOperand = string.Empty;
+            // remove trailing operand from expression
+            // simple approach: remove text after last operator
+            var expr = textBoxExpression.Text;
+            int idx = expr.Length - 1;
+            while (idx >= 0 && !IsOperator(expr[idx].ToString())) idx--;
+            textBoxExpression.Text = idx >= 0 ? expr.Substring(0, idx + 1) : string.Empty;
+            textBoxResult.Clear();
+        }
+
+        private void BtnDel_Click(object? sender, EventArgs e)
+        {
+            var expr = textBoxExpression.Text;
+            if (expr.Length == 0) return;
+            // remove last char
+            expr = expr.Substring(0, expr.Length - 1);
+            textBoxExpression.Text = expr;
+
+            // update currentOperand by scanning back from end until operator
+            int idx = expr.Length - 1;
+            var operand = string.Empty;
+            while (idx >= 0 && !IsOperator(expr[idx].ToString()))
+            {
+                operand = expr[idx] + operand;
+                idx--;
+            }
+            currentOperand = operand;
+            textBoxResult.Text = currentOperand;
+        }
+
+        private void BtnPM_Click(object? sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentOperand)) return;
+            if (currentOperand.StartsWith("-"))
+                currentOperand = currentOperand.Substring(1);
+            else
+                currentOperand = "-" + currentOperand;
+
+            // rebuild expression: replace last operand in expression
+            var expr = textBoxExpression.Text;
+            int idx = expr.Length - 1;
+            while (idx >= 0 && !IsOperator(expr[idx].ToString())) idx--;
+            textBoxExpression.Text = (idx >= 0 ? expr.Substring(0, idx + 1) : string.Empty) + currentOperand;
+            textBoxResult.Text = currentOperand;
+        }
+
+        private void BtnResult_Click(object? sender, EventArgs e)
+        {
+            // push last operand
+            if (!string.IsNullOrEmpty(currentOperand))
+            {
+                tokens.Add(currentOperand);
+            }
+
+            // build evaluable expression
+            var expr = textBoxExpression.Text.Replace("X", "*").Replace("x", "*").Replace("÷", "/");
+            try
+            {
+                var dt = new DataTable();
+                var value = dt.Compute(expr, null);
+                textBoxResult.Text = value.ToString();
+                // clear tokens and set currentOperand to result so further calculations can continue
+                tokens.Clear();
+                currentOperand = value.ToString();
+                textBoxExpression.Text = expr; // keep expression as entered (mapped operators)
+            }
+            catch
+            {
+                textBoxResult.Text = "Error";
+            }
+        }
+
+        private static bool IsOperator(string s)
+        {
+            return s == "+" || s == "-" || s == "X" || s == "x" || s == "*" || s == "÷" || s == "/";
+        }
+
+        private void textBoxExpression_TextChanged(object sender, EventArgs e)
+        {
+            // keep behavior minimal: when user types directly in expression, update current operand and result preview
+            var expr = textBoxExpression.Text;
+            int idx = expr.Length - 1;
+            var operand = string.Empty;
+            while (idx >= 0 && !IsOperator(expr[idx].ToString()))
+            {
+                operand = expr[idx] + operand;
+                idx--;
+            }
+            currentOperand = operand;
+            textBoxResult.Text = currentOperand;
         }
     }
 }
